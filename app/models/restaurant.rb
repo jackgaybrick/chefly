cd3f1df455c8e70a105f227805d4ec78f1c5b088
John Gaybrick <jack.gaybrick@gmail.com>
2015-05-28 15:03:56 -0400

    cleaned up restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,22 +1,12 @@
-require 'yaml'
+$LOAD_PATH << '.'
+require_relative '../services/neighborhoods'
 
 class Restaurant < ActiveRecord::Base
   has_many :dishes
   attr_accessor :this_restaurant
 
   BANNED_WORDS = ["bar", "drinks", "beverages", "wine", "beer", "vino", "desserts", "dolci", "tea", "coffee", "artiginali", "sweet wine", "port", "grappa", "madeira"]
-  NEIGHBORHOODS = {
-    central_harlem: [10026, 10027, 10030, 10037, 10039],
-    chelsea: [10001, 10011, 10018, 10019, 10020, 10036],
-    east_harlem: [10029, 10035],
-    gramercy: [10010, 10016, 10017, 10022],
-    soho: [10012, 10013, 10014],
-    lower_manhattan: [10004, 10005, 10006, 10007, 10038, 10280],
-    lower_east_side: [10002, 10003, 10009],
-    upper_east_side: [10021, 10028, 10044, 10065, 10075, 10128],
-    upper_west_side: [10023, 10024, 10025],
-    washington_heights: [10031, 10032, 10033, 10034, 10040]
-  }
+  include Neighborhoods
 
   def self.data_dump
     NEIGHBORHOODS.each do |hood,value|
